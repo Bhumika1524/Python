d3548fc62dc5b252ea26b6196f1e4001d06865bb
Bhumika1524 <51358927+Bhumika1524@users.noreply.github.com>
2021-02-08 19:09:44 +0530

    Area of circle and file extension

--- Area_of_Circle.py
+++ Area_of_Circle.py
@@ -2,3 +2,6 @@ import math
 radius = float(input("Input the radius of the circle: "))
 area = math.pi*radius**2
 print("The area of the circle with radius " +str(radius)+" is: "+str(area))
+
+filename = input("Input the Filename: ").split(".")
+print ("The extension of the file is : " +filename[-1])
