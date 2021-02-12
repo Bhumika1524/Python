61b06dc30f77c0970bc15885119b995457f8454b
Bhumika1524 <51358927+Bhumika1524@users.noreply.github.com>
2021-02-12 18:15:56 +0530

    Area of Circle and Filename Extensions

--- Area_of_Circle.py
+++ Area_of_Circle.py
@@ -1,7 +1,9 @@
+#Program to Calculate Area of Circle
 import math
 radius = float(input("Input the radius of the circle: "))
 area = math.pi*radius**2
 print("The area of the circle with radius " +str(radius)+" is: "+str(area))
 
+#Program to print Extensions from Filenames
 filename = input("Input the Filename: ").split(".")
 print ("The extension of the file is : " +filename[-1])
